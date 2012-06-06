5601b31f11bb9d3d5b8263fc8c3387871c701bf6
ksarien <ksarien@gmail.com>
2012-06-06 17:12:04 +0300

    add default controller, install gems: responders, twitter-bootstrap-rails, simple_form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
-class ApplicationController < ActionController::Base
-  protect_from_forgery
+require "application_responder"
+
+class ApplicationController < ActionController::Base  
+  protect_from_forgery  
+
+  self.responder = ApplicationResponder
+  respond_to :html
 end
